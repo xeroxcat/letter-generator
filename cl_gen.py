d66b6d8179bb949be8b91c5fab6f357941e1a482
Calvin Houser <xeroxcat@PSEUDOCRAFT.hsd1.ca.comcast.net>
2020-02-18 21:52:24 -0800

    init commit

---
+++ cl_gen.py
@@ -0,0 +1,40 @@
+
+template = '''\
+Dear {0} Hiring Team,
+
+I am excited to have found your open position for a {1}.
+I have recently graduated from UC Santa Cruz with a BS in computer science and am incredibly interested in the projects at {0} and this opportunity.
+I have had a persistent interest in {2} and I would relish the opportunity to professionally contribute to {3}.
+I greatly enjoy modeling problems and engineering their solutions.
+Data fragmentation is a problem I've regularly encountered in both my personal and professional life.
+I would relish the opportunity to contribute to an enterprise platform solution to this problem.
+The chance to make contributions to these systems makes this position too enticing to ignore.
+
+In the last year I have worked on a number of projects, both in school and on my own, which have given me the experience to comfortably prototype quickly with Python and operate in a Linux/Unix environment.
+Through schoolwork and personal projects I have developed {4}.
+In another class I worked with a team of four to build a distributed key-value store with replication and sharding in Docker.
+In this group I gained experience designing a REST API and leveraged my experience as a manager to confidently lead and structure the project.
+In the last year I have taken multiple classes based in C and Python and feel quite comfortable in both languages.
+In my last quarter before graduating, I designed a system to parse data from scanned grocery receipts and worked with a team to present aggregated data and create a graphical frontend for streamlining error correction in the image OCR process.
+I have some experience programming with C++ and Java as well.
+This second class was also an opportunity to learn about distributed algorithms for computation and consensus.
+I enjoy taking complex problems apart and working through their solutions in a collaborative social setting.
+In a personal project I worked with the Youtube API to collect all video IDs from a set of channels and play them through a customized web interface.
+This project gave me familiarity designing a project around an intuitive user experience.
+
+Since graduating I have been continuing to apply this process to my everyday problems, learning new platforms and frameworks on the way.
+I most recently wrote a python script for deduplicating data over a several personal device backups in preparation for setting up a proper backup server.
+This has given me increased comfort working in a remote Linux environment and spurred me to learn efficient ways of comparing and representing the differences between large sets of data.
+I {} recently learned the basics of Selenium to automate a complicated search pattern: finding nearby compatible car wheels across multiple makes.
+I am currently also building a digital synthesizer on the Arduino platform, gaining experience with physical circuit design and optimizing code for an embedded system.
+{} rewriting parts of an Arduino library to support I2C-like data transmission without calling delay functions. This project has given me experience with an embedded C++ environment and integrating new functionality into an existing interface.
+Currently I am working with the WebExtensions API to provide an external unified representation of active windows, bookmarks, and the associated browsing history in emacs org-mode.
+
+I am very excited for more opportunities to continue learning and working with a team of dedicated individuals. Thank you for reviewing my application and I hope to schedule an interview with you soon.
+
+Sincerely,
+Calvin Houser'''
+
+
+proficiency programming in both C and Python and a familiarity with OOP principles
+experience engineering on both sides of an API

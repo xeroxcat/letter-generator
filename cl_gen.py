cacd81ff8b7924019e6925d369dba8ffc29d1721
Calvin Houser <xeroxcat@PSEUDOCRAFT.hsd1.ca.comcast.net>
2020-02-19 19:26:50 -0800

    builds letter

--- cl_gen.py
+++ cl_gen.py
@@ -1,40 +1,161 @@
+#!/usr/bin/env python3
 
-template = '''\
-Dear {0} Hiring Team,
+from dialog import Dialog
+import locale
+import json
 
-I am excited to have found your open position for a {1}.
-I have recently graduated from UC Santa Cruz with a BS in computer science and am incredibly interested in the projects at {0} and this opportunity.
-I have had a persistent interest in {2} and I would relish the opportunity to professionally contribute to {3}.
-I greatly enjoy modeling problems and engineering their solutions.
-Data fragmentation is a problem I've regularly encountered in both my personal and professional life.
-I would relish the opportunity to contribute to an enterprise platform solution to this problem.
-The chance to make contributions to these systems makes this position too enticing to ignore.
+locale.setlocale(locale.LC_ALL, '')
 
-In the last year I have worked on a number of projects, both in school and on my own, which have given me the experience to comfortably prototype quickly with Python and operate in a Linux/Unix environment.
-Through schoolwork and personal projects I have developed {4}.
-In another class I worked with a team of four to build a distributed key-value store with replication and sharding in Docker.
-In this group I gained experience designing a REST API and leveraged my experience as a manager to confidently lead and structure the project.
-In the last year I have taken multiple classes based in C and Python and feel quite comfortable in both languages.
-In my last quarter before graduating, I designed a system to parse data from scanned grocery receipts and worked with a team to present aggregated data and create a graphical frontend for streamlining error correction in the image OCR process.
-I have some experience programming with C++ and Java as well.
-This second class was also an opportunity to learn about distributed algorithms for computation and consensus.
-I enjoy taking complex problems apart and working through their solutions in a collaborative social setting.
-In a personal project I worked with the Youtube API to collect all video IDs from a set of channels and play them through a customized web interface.
-This project gave me familiarity designing a project around an intuitive user experience.
+dwindow = Dialog(dialog='dialog', autowidgetsize=True)
+row_count, col_count = dwindow.maxsize()
+wrap_len = col_count - 30
 
-Since graduating I have been continuing to apply this process to my everyday problems, learning new platforms and frameworks on the way.
-I most recently wrote a python script for deduplicating data over a several personal device backups in preparation for setting up a proper backup server.
-This has given me increased comfort working in a remote Linux environment and spurred me to learn efficient ways of comparing and representing the differences between large sets of data.
-I {} recently learned the basics of Selenium to automate a complicated search pattern: finding nearby compatible car wheels across multiple makes.
-I am currently also building a digital synthesizer on the Arduino platform, gaining experience with physical circuit design and optimizing code for an embedded system.
-{} rewriting parts of an Arduino library to support I2C-like data transmission without calling delay functions. This project has given me experience with an embedded C++ environment and integrating new functionality into an existing interface.
-Currently I am working with the WebExtensions API to provide an external unified representation of active windows, bookmarks, and the associated browsing history in emacs org-mode.
+dwindow.set_background_title('MadLib')
 
-I am very excited for more opportunities to continue learning and working with a team of dedicated individuals. Thank you for reviewing my application and I hope to schedule an interview with you soon.
+#   open files
+# load spec file
+fieldspec = {}
+with open('fields.json') as f:
+    fieldspec = json.load(f)
 
-Sincerely,
-Calvin Houser'''
+if 'default_exclude' in fieldspec:
+    exclude_defaults = fieldspec['default_exclude']
 
+template_lines = []
+with open('cl_template.txt') as f:
+    template_lines = [(str(i), line.replace('\n', ''))
+                      for i, line in enumerate(f.readlines())]
 
-proficiency programming in both C and Python and a familiarity with OOP principles
-experience engineering on both sides of an API
+#   select lines for each paragraph
+paragraphs = []
+first_line = 0
+for idx, line in template_lines:
+    if not line.strip():
+        paragraphs.append(template_lines[first_line:int(idx)])
+        first_line = int(idx) + 1
+
+excluded_lines = []
+for i, p in enumerate(paragraphs):
+    line_choices = [(idx, line, True)
+                    if idx in exclude_defaults else (idx, line, False)
+                    for idx, line in p]
+    for idx, entry in enumerate(line_choices):
+        ltag, line, lbool = entry
+        if len(line) > wrap_len:
+            display_line = line
+            l_break = line[:wrap_len].rfind(' ')
+            display_line = display_line[:l_break] + '[...]'
+            line_choices[idx] = (ltag, display_line, lbool, line)
+        else:
+            line_choices[idx] = (*entry, line)
+    code, t_list = dwindow.checklist(title='Paragraph ' + str(i),
+                                     text='Select which lines to exclude',
+                                     choices=line_choices,
+                                     cr_wrap=True, item_help=True,
+                                     help_button=True, 
+                                     help_label='See Full Line',
+                                     help_status=True)
+    while code == Dialog.HELP:
+        dwindow.msgbox(t_list[0])
+        code, t_list = dwindow.checklist(title='Paragraph ' + str(i),
+                                     text='Select which lines to exclude',
+                                     choices=t_list[2],
+                                     cr_wrap=True, item_help=True,
+                                     help_button=True,
+                                     help_label='See Full Line',
+                                     help_status=True)
+    excluded_lines.extend(t_list)
+
+#   option to resave lines to exclude
+if dwindow.yesno(str(excluded_lines), title="Excluded Lines",
+                 yes_label="Continue", no_label="Save as Default") != Dialog.OK:
+    fieldspec['default_exclude'] = excluded_lines
+    with open('fields.json', 'w') as f:
+        json.dump(fieldspec, f, indent=4, separators=(',', ': '))
+
+#   exclude selected lines from template
+included_lines = [line[1] for line in template_lines
+                  if line[0] not in excluded_lines]
+
+#   find a line of context for each entry point
+fields = fieldspec.get('fields', [])
+entries = {i: {} for i in range(len(fields))}
+for i in entries:
+    for idx, line in enumerate(included_lines):
+        #dwindow.msgbox(str(line))
+        fill = '{' + str(i) + '}'
+        if fill in line:
+            entries[i]['context'] = ((included_lines[idx-1] if idx != 0 else '')
+                                     + ' \Z5' + line.replace(fill, '\Zb<'
+                                                    + fields[i]['title']
+                                                    + '>\ZB')
+                                     + '\Zn')
+            break
+
+#fill entry points
+new_field_vals = {}
+for idx, field in enumerate(fields):
+    for line in included_lines:
+        if '{' + str(idx) + '}' in line:
+            break
+    #field isn't present in the document
+    else:
+        entries[idx]['value'] = 'n/a'
+        continue
+
+    if 'options' in field:
+        opts = field['options'] + ['<add new>']
+        idxs = [str(i) for i in range(len(opts))]
+        ret, choice = dwindow.menu(title=field['title'],
+                                   text=entries[idx]['context'],
+                                   colors=True,
+                                   choices=zip(idxs, opts))
+        #enter custom selection if <add new> was selected
+        if choice == str(len(opts) - 1):
+            ret, choice = dwindow.inputbox(title=field['title'],
+                                           text=entries[idx]['context'],
+                                           colors=True,
+                                           extra_button=True,
+                                           extra_label='Save as Option')
+        else:
+            choice = opts[int(choice)]
+    else:
+        ret, choice = dwindow.inputbox(title=field['title'],
+                                       text=entries[idx]['context'],
+                                       colors=True,
+                                       extra_button=True,
+                                       extra_label='Save as Option')
+    result = '' if '<none>' in choice else choice
+    if ret == Dialog.EXTRA:
+        new_field_vals[idx] = choice
+    entries[idx]['value'] = result
+
+#   save any new field vals if requested
+for idx, val in new_field_vals.items():
+    if 'options' in fieldspec['fields'][idx]:
+        fieldspec['fields'][idx]['options'].append(val)
+    else:
+        fieldspec['fields'][idx]['options'] = [val]
+
+if new_field_vals:
+    if dwindow.yesno(str(new_field_vals), title="Save New Options",
+                    yes_label="Continue without Saving",
+                    no_label="Save to Default") != Dialog.OK:
+        fieldspec['default_exclude'] = excluded_lines
+        with open('fields.json', 'w') as f:
+            json.dump(fieldspec, f, indent=4, separators=(',', ': '))
+
+#   format output and print
+field_vals = [entry['value'] for entry in entries.values()]
+#dwindow.msgbox(''.join(included_lines) + str(field_vals))
+for idx, line in enumerate(included_lines):
+    if not line or line[-1] != '.':
+        included_lines[idx] = line + '\n'
+    else:
+        included_lines[idx] = line + ' '
+output = ''.join(included_lines).format(*field_vals)
+dwindow.msgbox(output)
+
+print()
+print(output)
+
